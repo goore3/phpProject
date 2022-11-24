717434848dfa066d278044d3c23c45e562e671ca
goore3 <38388042+goore3@users.noreply.github.com>
2022-11-24 21:43:41 +0000

    Update login.php

--- moviesapp/app/views/user/login.php
+++ moviesapp/app/views/user/login.php
@@ -3,23 +3,22 @@
 ?>
 
     <div class="container">
-    
-        <h1>Login into your account:</h1>
-
+      <h1>Login into your account:</h1>
       <div class="row row-cols-1 row-cols-md-3 g-4">
-        
         <form method="POST" action="<?php echo PROJECT_URL; ?>/user/login">
             <label>Username:</label>
             <input required type="text" name="username">
             <br>
             <br>
             <label>Password:</label>
-            <input required type="password" name="password">
+            <input required type="password" name="password"><br>
             <button type="submit">Login</button>
         </form> 
-
       </div>
-      
+      <div>
+        Dont have an account? 
+      </div>
+      <a href="<?php echo PROJECT_URL; ?>/user/register">Register</a>
     </div>
 
 <?php
