38ddf0cde3ff99fd3af57445191278f304bb1034
goore3 <38388042+goore3@users.noreply.github.com>
2022-11-24 12:22:52 +0000

    Update get.php

--- moviesapp/app/views/movie/get.php
+++ moviesapp/app/views/movie/get.php
@@ -6,17 +6,17 @@
     
       <h1>Showing details of movie: <?php echo $data['movies'][0]['title']; ?> </h1>
 
-      <div class="row row-cols-1 row-cols-md-3 g-4">
+      <!-- <div class="row row-cols-1 row-cols-md-3 g-4"> -->
 
         <?php
-          echo "<ul>";
           foreach ($data['movies'] as $movie) {
-            echo '<li>' . $movie['title'] . '</li>';
+            echo '<br><h2>Description:</h2><p>' . $movie['description'] . '</p>';
+            echo '<h5>IMDB Rating:</h5><p>' . $movie['imdb_rating'] . '</p>';
+            echo '<h5>Release Date:</h5><p>' . $movie['release_year'] . '</p>';
           }
-          echo "<ul>";
         ?>
       
-      </div>
+      <!-- </div> -->
 
 <?php
   require SERVER_PATH.'\app\views\_includes\footer.php';
